5a1d93865ce49644782bdcf34a327002941879da
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-07-30 09:50:00 +0000

    Modify run.3.py to async version

--- 201510_python/actions.py
+++ 201510_python/actions.py
@@ -4,15 +4,28 @@ import sys
 import time
 import threading
 
+g_lock = threading.Lock()
+g_sensor_values = [0,0,0,0]
+
 def motor(l_hz,r_hz,msec):
 	f = open("/dev/rtmotor0","w")
 	print >> f, "%d %d %d" % (r_hz,l_hz,msec)
 	f.close()
 
+def lightsensorLock():
+	global g_lock
+	global g_sensor_values
+	with g_lock:
+		f = open("/dev/rtlightsensor0","r")
+		vs = f.readline().rstrip().split()
+		f.close()
+		g_sensor_values = [ int(s) for s in vs ]
+
 def lightsensor():
 	f = open("/dev/rtlightsensor0","r")
 	vs = f.readline().rstrip().split()
 	f.close()
+
 	return [ int(s) for s in vs ]
 
 def forward(distance):
@@ -35,12 +48,15 @@ def turn(deg):
 		r_hz, l_hz, tm = -r_hz, -l_hz, -tm
 
 	motor(l_hz,r_hz,tm)
-		
-def curve_to_wall(direction):
 
-	while sum(lightsensor()) < 500:
-		r_hz = 450
-		l_hz = 450
+def curve_to_wall(direction):
+	global g_sensor_values
+
+	while True:
+		t = threading.Thread(target=lightsensorLock)
+		t.start()
+
+		r_hz, l_hz = 450, 450
 		tm = 100
 		if direction == "R":
 			l_hz = l_hz + 50
@@ -49,9 +65,18 @@ def curve_to_wall(direction):
 
 		motor(l_hz,r_hz,tm)
 
+		t.join()
+		if sum(g_sensor_values) > 500:
+			return
+
+
 def search_space(direction):
+	global g_sensor_values
 
 	while True:
+		t = threading.Thread(target=lightsensorLock)
+		t.start()
+
 		r_hz, l_hz = 300, -300 
 		tm = 100
 		if direction == "R":
@@ -59,7 +84,7 @@ def search_space(direction):
 
 		motor(l_hz,r_hz,tm)
 
-		vs = lightsensor()
-		if vs[0] + vs[3] < 200:
-			break
+		t.join()
+		if g_sensor_values[0] + g_sensor_values[3] < 200:
+			return
 
