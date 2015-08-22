7106fe943de1f9676b07d13d65e6b0f94d0963cf
Ryuichi Ueda <ryuichiueda@gmail.com>
2015-08-22 10:02:50 +0000

    Modify run.bash for 201511

--- 201511/run.bash
+++ 201511/run.bash
@@ -4,8 +4,6 @@ source "$(dirname $(readlink -f $0))/actions"
 tmp=/run/shm/tmp$$
 log=/home/pi/log
 
-: > $log
-
 balance () {
 	while : ; do
 		awk '$2+$3>1500{print $2-$3}' /dev/rtlightsensor0	|
@@ -13,18 +11,18 @@ balance () {
 
 		case "$(cat $tmp-dir)" in
 			"" ) return ;;
-			"R" ) echo 150 -150 100 | tee -a $log > /dev/rtmotor0 ;;
-			"L" ) echo -150 150 100 | tee -a $log > /dev/rtmotor0 ;;
+			"R" ) turn -3 ;;
+			"L" ) turn 3 ;;
 		esac
-		cat < /dev/rtlightsensor0 >> $log
+		cat /dev/rtlightsensor0
 	done
 }
 
 forward_to_wall () {
 	while awk '$1>2000 || $4>2000{exit 1}' /dev/rtlightsensor0 ; do
 		balance
-		echo 300 300 300 | tee -a $log > /dev/rtmotor0
-		cat < /dev/rtlightsensor0 >> $log
+		forward 50
+		cat /dev/rtlightsensor0
 	done
 }
 
@@ -32,4 +30,4 @@ forward_to_wall () {
 while awk '$1>500&&$2>500&&$3>500&&$4>500{exit 1}' /dev/rtlightsensor0 ; do
 	forward_to_wall
 	turn -90
-done
+done > $log
